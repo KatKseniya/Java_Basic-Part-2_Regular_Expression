638d4a3d03b5096329265413f7606fad36893d7a
KatKseniya <91549260+KatKseniya@users.noreply.github.com>
2022-02-01 13:26:28 +0300

    Update Regular_Expression15.java

--- regexpres/Regular_Expression15.java
+++ regexpres/Regular_Expression15.java
@@ -1,10 +1,5 @@
 package regexpres;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Regular_Expression15 {
     public static void main(String[] args) {
 
